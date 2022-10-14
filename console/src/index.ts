914c566a66e179e23d2c6e1d6ea5f005939f8809
Nanxu <47135624+pannanxu@users.noreply.github.com>
2022-10-14 20:23:02 +0800

    Initial commit

---
+++ console/src/index.ts
@@ -0,0 +1,40 @@
+import { definePlugin, BasicLayout } from "@halo-dev/console-shared";
+import DefaultView from "./views/DefaultView.vue";
+import { IconGrid } from "@halo-dev/components";
+import "./styles/index.css";
+
+export default definePlugin({
+  name: "PluginStarter",
+  components: [],
+  routes: [
+    {
+      path: "/hello-world",
+      component: BasicLayout,
+      children: [
+        {
+          path: "",
+          name: "HelloWorld",
+          component: DefaultView,
+          meta: {
+            permissions: ["plugin:apples:view"],
+          },
+        },
+      ],
+    },
+  ],
+  menus: [
+    {
+      name: "From PluginStarter",
+      items: [
+        {
+          name: "HelloWorld",
+          path: "/hello-world",
+          icon: IconGrid,
+        },
+      ],
+    },
+  ],
+  extensionPoints: {},
+  activated() {},
+  deactivated() {},
+});
